b4803494b5c0233bb47cc56d9d674566ed07ba39
ofirperez11 <125492890+ofirperez11@users.noreply.github.com>
2025-08-17 20:17:14 +0300

    Create def.h

---
+++ controller/def.h
@@ -0,0 +1,14 @@
+#ifndef _DEF_H_
+#define _DEF_H_
+
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+#define cmndForwardRight  0x30
+#define cmndForwardLeft   0x31
+#define cmndForward       0x32
+#define cmndBackward      0x33
+
+#endif
